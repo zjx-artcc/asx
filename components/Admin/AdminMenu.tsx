419ab963e32c3b32b2628953ca1987cdf94869d4
Aneesh Reddy <aneeshjreddy@gmail.com>
2025-02-04 00:20:49 -0500

    Updated header for admin menu to read A.S.X. not I.D.S.

--- components/Admin/AdminMenu.tsx
+++ components/Admin/AdminMenu.tsx
@@ -7,7 +7,7 @@ export default function AdminMenu() {
     return (
         <Card>
             <CardContent>
-                <Typography variant="h6" textAlign="center">I.D.S. Management</Typography>
+                <Typography variant="h6" textAlign="center">A.S.X. Management</Typography>
                 <List>
                     <Link href="/admin" style={{textDecoration: 'none', color: 'inherit',}}>
                         <ListItemButton>
